05df21eb149d84b5865064ea178700515cfe7979
Branimir <rickobranimir@gmail.com>
2016-07-30 07:49:59 +0200

    Commands

---
+++ MultyNetHack/MultyNetHack/Commands/GeneratePathCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultyNetHack.Commands
+{
+    public class GeneratePathCommand : BaseCommand
+    {
+        public int NumberOfPaths;
+
+    }
+}
