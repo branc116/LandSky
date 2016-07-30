39eb2c5d52803db7fd724ed81eb4b9ee24f281e4
Branimir <rickobranimir@gmail.com>
2016-07-30 07:57:27 +0200

    DMS

---
+++ MultyNetHack/MultyNetHack/DebugItems/DebugMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultyNetHack.DebugItems
+{
+    public class DebugMessage
+    {
+        public DateTime CreateTime;
+        public string Message;
+        public DebugMessage(string Message)
+        {
+            this.Message = Message;
+            CreateTime = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0})>{1}", CreateTime, Message);
+        }
+    }
+}
