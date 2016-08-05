fe4e250847f98c21c6de61411a8b242db8b637c6
Branimir <rickobranimir@gmail.com>
2016-08-05 04:38:03 +0200

    New constructor

--- MultyNetHack/MultyNetHack/Components/Walls.cs
+++ MultyNetHack/MultyNetHack/Components/Walls.cs
@@ -8,23 +8,33 @@ namespace MultyNetHack.Components
     /// </summary>
     public class HorizontalWall : Component
     {
+        /// <summary>
+        /// Deprecated!!!
+        /// </summary>
         public HorizontalWall(string name, Point centerLoc, int sizeLeft, int sizeRight) : base(name)
         {
             IsPassable = false;
             LocalX = centerLoc.x;
             LocalY = centerLoc.y;
             MadeOf = Material.HorisontalWall;
-            Height = 1;
-            Width = sizeLeft + sizeRight + 1;
+            
         }
+        /// <summary>
+        /// Deprecated!!!
+        /// </summary>
         public HorizontalWall(string name, Point centerLoc, int size) : base(name)
-        {
+        { 
             IsPassable = false;
             LocalX = centerLoc.x;
             LocalY = centerLoc.y;
             MadeOf = Material.HorisontalWall;
-            Height = 1;
-            Width = size;
+            
+        }
+        public HorizontalWall(string Name, Rectangle Bounds) : base(Name)
+        {
+            mBounds = Bounds;
+            IsPassable = false;
+            MadeOf = Material.HorisontalWall;
         }
     }
     /// <summary>
@@ -32,23 +42,45 @@ namespace MultyNetHack.Components
     /// </summary>
     public class VerticalWall : Component
     {
+        /// <summary>
+        /// Deprecated!!!
+        /// </summary>
         public VerticalWall(string name, Point location, int sizeUp, int sizeDown) : base(name)
         {
             IsPassable = false;
             LocalX = location.x;
             LocalY = location.y;
-            Width = 1;
-            Height = sizeUp + sizeDown;
+            
             MadeOf = Material.VerticalWall;
         }
+        /// <summary>
+        /// Deprecated!!!
+        /// </summary>
         public VerticalWall(string name, Point location, int size) : base(name)
         {
             IsPassable = false;
             LocalX = location.x;
             LocalY = location.y;
-            Width = 1;
-            Height = size;
+            
             MadeOf = Material.VerticalWall;
+        }
+        public VerticalWall(string Name, Rectangle Bounds) : base(Name)
+        {
+            mBounds = Bounds;
+            IsPassable = false;
+            MadeOf = Material.HorisontalWall;
+        }
+    }
+    public class Wall : Component
+    {
+        public Wall (string Name, Rectangle Bounds) : base(Name)
+        {
+            if (Bounds.Width > Bounds.height)
+                MadeOf = Material.HorisontalWall;
+            else
+                MadeOf = Material.VerticalWall;
+            mBounds = Bounds;
+            IsPassable = false;
         }
     }
 }
