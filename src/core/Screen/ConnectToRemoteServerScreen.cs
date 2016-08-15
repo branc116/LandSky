d145c3d51a96a4eae5d295006e550969651e02ce
Branimir <rickobranimir@gmail.com>
2016-08-15 02:24:01 +0200

    Added Screen to connect to remote server

---
+++ src/core/Screen/ConnectToRemoteServerScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LandSky.UIComponents;
+
+namespace LandSky.Screen
+{
+    public class ConnectToRemoteServerScreen : BaseScreen
+    {
+
+        public ConnectToRemoteServerScreen() : base(0,0,"Connect To Remote Server")
+        {
+            InitTextBoxes();
+            ScreenChange();
+        }
+
+        private void InitTextBoxes()
+        {
+            UIComponents.Add(new TextBox("Ip", 0, 7, 7));
+        }
+    }
+}
